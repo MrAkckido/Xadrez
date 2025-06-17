e779c53ee0c0caa611dae74cf8b712939b409eea
MrAkckido <akckidkid@gmail.com>
2025-06-17 17:51:23 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -5,28 +5,27 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+    //mover torre 5 casas para direita
+    printf("Movimento da torre:\n");
+    for(int i = 0; i < 5; i++){
+        printf("Direita\n");//imprime a direção do movimento
+    }
+    printf("\n\n");
+    //bispo move 5 casas na diagonal pra cima e pra direita
+    printf("Movimento do bispo:\n");
+    int i = 0;
+    do{
+    printf("Cima, direita\n");//imprime a direção do movimento
+    i++;
+    }while(i <= 5);//condição pro loop
+    printf("\n\n");
+    //rainha move 8 casas pra esquerda
+    i = 0;
+    printf("movimento da rainha:\n");
+    while(i <= 8){
+        printf("Esquerda\n");//imprime o movimento
+        i++;
+    }
 
     return 0;
 }
